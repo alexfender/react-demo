abfde95b626d10e565dcd59f27be65328632691b
alexfender <alexfender@mail.ru>
2021-04-19 14:20:37 +0300

    Add getOrders, orders table, pagination, orders per page, filter

--- src/pages/OrdersPage.tsx
+++ src/pages/OrdersPage.tsx
@@ -2,8 +2,43 @@ import React from 'react'
 
 const OrdersPage:React.FC = () => {
   return (
-    <div>
-      Заказы
+    <div className="card card-custom gutter-b">
+      <div className="card-header">
+        <div className="card-title">
+          <h3 className="card-label">Список заказов</h3>
+        </div>
+        <div className="card-toolbar">
+          <button type="button" className="btn btn-primary">Новый заказ</button>
+        </div>
+      </div>
+      <div className="card-body">
+        <form className="form form-label-right">
+        </form>
+        <div className="react-bootstrap-table table-responsive">
+          <table className="table table table-head-custom table-vertical-center overflow-hidden">
+            <thead>
+              <tr>
+                <th className="selection-cell-header" data-row-selection="true">
+                  <label className="checkbox checkbox-single">
+                    <input type="checkbox" /><span></span>
+                  </label>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="selection-cell">
+                  <label className="checkbox checkbox-single">
+                    <input type="checkbox" />
+                    <span></span>
+                  </label>
+                </td>
+                <td>1</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
